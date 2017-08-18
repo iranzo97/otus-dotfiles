0160ba93def16636325c0886cfe6b94c4ad80320
Pilonsi <jorgepil508@gmail.com>
2017-08-18 22:11:56 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo Copying configuration files...
+cp i3scripts ~/.i3scripts
+cp i3/config ~/.config/i3/
+cp polybar/config ~/.config/polybar/
+cp oh-my-zsh/pilonsi.zsh-theme ~/.oh-my-zsh/themes/
+cp cmus/pilonsi.theme ~/.config/cmus/
+cp zsh/zshrc ~/.zshrc
+cp xresources/.Xresources ~/
+cp xsession/.xsession ~/
+echo Done! We recommend to double check the script launchers such as .xsession or the i3 config file before launch. Enjoy :)
